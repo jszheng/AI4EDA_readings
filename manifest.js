1d90733d6012b451d32010141e744c3eeec1c73f
jszheng <zheng_js@hotmail.com>
2026-06-16 06:11:20 +0800

    auto update: 2026-06-16

--- manifest.js
+++ manifest.js
@@ -4,14 +4,15 @@
  */
 window.SITE_MANIFEST = {
   stats: {
-    papers: 157,
-    batches: 67,
-    news: 107,
-    lastUpdate: "2026-06-15"
+    papers: 160,
+    batches: 68,
+    news: 109,
+    lastUpdate: "2026-06-16"
   },
 
   /* 侧边栏「按日期」视图 */
   latest: [
+    { date: "2026-06-16", title: "🔥 06-16 最新",  file: "content/latest/2026-06-16.html" },
     { date: "2026-06-15", title: "🔥 06-15 最新",  file: "content/latest/2026-06-15.html" },
     { date: "2026-06-12", title: "🔥 06-12 最新",  file: "content/latest/2026-06-12.html" },
     { date: "2026-06-11", title: "🔥 06-11 最新",  file: "content/latest/2026-06-11.html" },
@@ -43,6 +44,7 @@ window.SITE_MANIFEST = {
   ],
 
   dates: [
+    { date: "2026-06-16", title: "2026-06-16", count: 5,  file: "content/dates/2026-06-16.html" },
     { date: "2026-06-15", title: "2026-06-15", count: 3,  file: "content/dates/2026-06-15.html" },
     { date: "2026-06-12", title: "2026-06-12", count: 3,  file: "content/dates/2026-06-12.html" },
     { date: "2026-06-11", title: "2026-06-11", count: 5,  file: "content/dates/2026-06-11.html" },
