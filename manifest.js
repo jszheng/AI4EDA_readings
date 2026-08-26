468a0694721798e3b1f9c50bb127e370baf765a7
jszheng <zheng_js@hotmail.com>
2026-08-27 05:18:20 +0800

    auto update: 2026-08-27

--- manifest.js
+++ manifest.js
@@ -4,9 +4,9 @@
  */
 window.SITE_MANIFEST = {
   stats: {
-    papers: 298,
+    papers: 299,
     batches: 113,
-    news: 203,
+    news: 205,
     lastUpdate: "2026-08-27"
   },
 
@@ -92,7 +92,7 @@ window.SITE_MANIFEST = {
   ],
 
   dates: [
-    { date: "2026-08-27", title: "2026-08-27", count: 2, file: "content/dates/2026-08-27.html" },
+    { date: "2026-08-27", title: "2026-08-27", count: 3, file: "content/dates/2026-08-27.html" },
     { date: "2026-08-26", title: "2026-08-26", count: 4, file: "content/dates/2026-08-26.html" },
     { date: "2026-08-25", title: "2026-08-25", count: 1, file: "content/dates/2026-08-25.html" },
     { date: "2026-08-24", title: "2026-08-24", count: 6, file: "content/dates/2026-08-24.html" },
