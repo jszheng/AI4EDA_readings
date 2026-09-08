d024218aa24cea9e0bdd17720047a07b3429858c
jszheng <zheng_js@hotmail.com>
2026-09-09 05:11:17 +0800

    auto update: 2026-09-09

--- manifest.js
+++ manifest.js
@@ -4,14 +4,15 @@
  */
 window.SITE_MANIFEST = {
   stats: {
-    papers: 325,
-    batches: 121,
-    news: 215,
-    lastUpdate: "2026-09-08"
+    papers: 327,
+    batches: 122,
+    news: 216,
+    lastUpdate: "2026-09-09"
   },
 
   /* 侧边栏「按日期」视图 */
   latest: [
+    { date: "2026-09-09", title: "🔥 09-09 最新",  file: "content/latest/2026-09-09.html" },
     { date: "2026-09-08", title: "🔥 09-08 最新",  file: "content/latest/2026-09-08.html" },
     { date: "2026-09-07", title: "🔥 09-07 最新",  file: "content/latest/2026-09-07.html" },
     { date: "2026-09-04", title: "🔥 09-04 最新",  file: "content/latest/2026-09-04.html" },
@@ -100,6 +101,7 @@ window.SITE_MANIFEST = {
   ],
 
   dates: [
+    { date: "2026-09-09", title: "2026-09-09", count: 2, file: "content/dates/2026-09-09.html" },
     { date: "2026-09-08", title: "2026-09-08", count: 1, file: "content/dates/2026-09-08.html" },
     { date: "2026-09-07", title: "2026-09-07", count: 4, file: "content/dates/2026-09-07.html" },
     { date: "2026-09-04", title: "2026-09-04", count: 2, file: "content/dates/2026-09-04.html" },
